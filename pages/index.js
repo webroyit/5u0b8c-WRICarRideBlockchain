a314f94c15680285cf896abf1e922045d9dae7ee
royweb <xytank@gmail.com>
2022-05-17 14:52:49 -0400

    Add Location Selector

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Navbar from "../components/Navbar"
 import Map from "../components/Map"
+import LocationSelector from "../components/LocationSelector"
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
-  main: `h-full w-screen flex-1 z-10`
+  main: `h-full w-screen flex-1 z-10`,
+  mapContainer: `flex-1 w-full h-full`,
+  rideRequestContainer: `h-full w-[400px] ml-[1rem] py-[3rem] absolute top-0 left-0 flex flex-col justify-end z-20`,
+  rideRequest: `h-full max-h-[700px] bg-white rounded-lg flex flex-col overflow-scroll`,
 }
 
 export default function Home() {
@@ -13,8 +17,11 @@ export default function Home() {
       <div className={style.main}>
         <Map />
       </div>
-      
-      <h1>WRI Car Ride Blockchain</h1>
+      <div className={style.rideRequestContainer}>
+        <div className={style.rideRequest}>
+          <LocationSelector />
+        </div>
+      </div>
     </div>
   )
 }
