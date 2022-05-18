a9267b58708db80b7bbd080b47403f9a9ef18cf7
royweb <xytank@gmail.com>
2022-05-18 12:13:21 -0400

    Add Confirm

---
+++ components/Confirm.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const style = {
+  wrapper: `flex-1 h-full flex flex-col justify-between`,
+  rideSelectorContainer: `h-full flex flex-col overflow-scroll`,
+  confirmButtonContainer: ` border-t-2 cursor-pointer z-10`,
+  confirmButton: `bg-black text-white m-4 py-4 text-center text-xl`,
+}
+
+const Confirm = () => {
+  const storeTripDetails = async () => {
+
+  }
+  return (
+    <div className={style.wrapper}>
+      <div className={style.rideSelectorContainer}></div>
+      <div className={style.confirmButtonContainer}>
+        <div 
+          className={style.confirmButton}
+          onClick={() => storeTripDetails()}
+        >
+          Confirm WRICRBC X
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Confirm
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar"
 import Map from "../components/Map"
 import LocationSelector from "../components/LocationSelector"
+import Confirm from "../components/Confirm"
 
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
@@ -20,6 +21,7 @@ export default function Home() {
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
           <LocationSelector />
+          <Confirm />
         </div>
       </div>
     </div>
